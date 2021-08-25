72d6ba28243209271e5fad52829d48143b4f0b36
bentoMiguel <82425298+bentoMiguel@users.noreply.github.com>
2021-08-25 09:52:49 -0300

    Criação da classe de Conexão

--- LojasNews/src/uteis/Conexao.java
+++ LojasNews/src/uteis/Conexao.java
@@ -1,7 +1,24 @@
 
 package uteis;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Conexao {
+    private static final String url = "jdbc:myslq://localhost:3306/nome_do_banco";
+    private static final String user = "seu_usuario";
+    private static final String password = "sua_senha";
     
+    public static Connection getConnection() {
+        Connection conexao = null;
+
+		try {
+			conexao = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conexao;
+    }
 }
